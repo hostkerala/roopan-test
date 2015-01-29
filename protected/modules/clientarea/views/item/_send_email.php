fd76d9b4fc9caf8cc7b01f315e419be7819cc504
Roopz <yiioverflow@gmail.com>
2015-01-29 21:09:18 +0530

    duplicate submit button fixed

--- protected/modules/clientarea/views/item/_send_email.php
+++ protected/modules/clientarea/views/item/_send_email.php
@@ -133,29 +133,16 @@
                 <hr />  
                 <?php echo $form->hiddenField($model,'selected_items');  ?>
                 <?php echo CHtml::submitButton('Submit', array('class' => 'btn btn-primary')); ?>            
-                <?php echo CHtml::submitButton('Cancel', array('class' => 'btn btn-danger')); ?>
+                <?php echo CHtml::link('Cancel', $this->createAbsoluteUrl('/clientarea/item/index'),array('class'=>'btn btn-danger', 'id'=>'cancel_btn')); ?>
             </div>
         </div>
     </div>
 </div>
-<script type="text/javascript">
-//$(document).ready(function() {
-//tinymce.init({
-//    selector: "textarea",
-//    theme: "modern",
-//    plugins: [
-//        "advlist autolink lists link image charmap print preview hr anchor pagebreak",
-//        "searchreplace wordcount visualblocks visualchars code fullscreen",
-//        "insertdatetime media nonbreaking save table contextmenu directionality",
-//        "emoticons template paste textcolor colorpicker textpattern"
-//    ],
-//    toolbar1: "insertfile undo redo | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image",
-//    toolbar2: "print preview media | forecolor backcolor emoticons",
-//    image_advtab: true,
-//    templates: [
-//        {title: 'Test template 1', content: 'Test 1'},
-//        {title: 'Test template 2', content: 'Test 2'}
-//    ]
-//});
-//})
-</script>
+
+<style>
+    #cancel_btn
+    {
+        font-size: 24px;       
+    }
+</style>
+
